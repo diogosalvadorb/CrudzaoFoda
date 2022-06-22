2f23a83e006f824519c508b769ce300279f7dafc
diogosalvadorb <105182603+diogosalvadorb@users.noreply.github.com>
2022-06-21 22:39:41 -0300

    Controller metodo Get, Post

---
+++ src/CrudzaoFoda/CrudzaoFoda.API/Controllers/FilmeController.cs
@@ -0,0 +1,51 @@
+﻿using CrudzaoFoda.Application.Contratos;
+using CrudzaoFoda.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudzaoFoda.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilmeController : Controller
+    {   
+        private readonly IFilmeService _filmeService;
+        public FilmeController(IFilmeService filmeService)
+        {
+            _filmeService = filmeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var filme = await _filmeService.GetAllFilmesAsync();
+                if (filme == null) return NotFound("Nenhum Filme encontrado.");
+
+                return Ok(filme);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Filme model)
+        {
+            try
+            {
+                var filme = await _filmeService.AddFilme(model);
+                if (filme == null) return BadRequest("Erro ao adicionar Filme");
+
+                return Ok(filme);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+    }
+}
--- src/CrudzaoFoda/CrudzaoFoda.API/Program.cs
+++ src/CrudzaoFoda/CrudzaoFoda.API/Program.cs
@@ -1,4 +1,8 @@
+using CrudzaoFoda.Application;
+using CrudzaoFoda.Application.Contratos;
+using CrudzaoFoda.Persistence;
 using CrudzaoFoda.Persistence.Contexto;
+using CrudzaoFoda.Persistence.Contratos;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +12,8 @@ builder.Services.AddDbContext<FilmeDbContexto>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FilmeDbContexto")));
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IFilmeService, FilmeService>();
+builder.Services.AddScoped<IFilmesPersist, FilmesPersist>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
